5b72d6f16c1db2a1aa82d8dd4247b0e10732b3f8
pradeepiOS <pradeep.iostech@gmail.com>
2021-11-05 17:58:39 +0530

    Initial Commit

---
+++ UserLocationUsingSwiftUI/UserLocationUsingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationUsingSwiftUIApp.swift
+//  UserLocationUsingSwiftUI
+//
+//  Created by Pradeep's Macbook on 05/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationUsingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
